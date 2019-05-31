e83ec87e437859a311d5047b46b8c0e504e0c10b
Luca Molteni <volothamp@gmail.com>
2019-05-31 13:37:47 +0200

    Two cells

--- src/main/java/com/antempire/Cell.java
+++ src/main/java/com/antempire/Cell.java
@@ -3,6 +3,13 @@ package com.antempire;
 public class Cell {
 
     private boolean isProducing = false;
+    private final int x;
+    private final int y;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
     public boolean isProducing() {
         return isProducing;
@@ -10,5 +17,13 @@ public class Cell {
 
     public void setProducing(boolean producing) {
         isProducing = producing;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 }
--- src/test/java/com/antempire/AntEmpireTest.java
+++ src/test/java/com/antempire/AntEmpireTest.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015 Red Hat, Inc. and/or its affiliates.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-*/
-
 package com.antempire;
 
 import org.junit.Test;
@@ -25,17 +10,19 @@ import static org.junit.Assert.*;
 public class AntEmpireTest {
 
     @Test
-    public void testKJar() throws Exception {
+    public void testKJar() {
         KieServices ks = KieServices.Factory.get();
         KieContainer kContainer = ks.getKieClasspathContainer();
         KieSession kSession = kContainer.newKieSession("AntEmpireKBase.session");
 
 
-        Cell c = new Cell();
+        Cell center = new Cell(4, 4);
+        Cell right = new Cell(5, 0);
 
-        kSession.insert(c);
+        kSession.insert(center);
+        kSession.insert(right);
 
         int rules = kSession.fireAllRules();
-        assertEquals(1, rules);
+        assertEquals(2, rules);
     }
 }
