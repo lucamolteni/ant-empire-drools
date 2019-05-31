5fb4869867c7063cb4129448d175ff78e02d71c6
Luca Molteni <volothamp@gmail.com>
2019-05-31 13:33:53 +0200

    First commit

---
+++ src/main/java/com/antempire/Cell.java
@@ -0,0 +1,14 @@
+package com.antempire;
+
+public class Cell {
+
+    private boolean isProducing = false;
+
+    public boolean isProducing() {
+        return isProducing;
+    }
+
+    public void setProducing(boolean producing) {
+        isProducing = producing;
+    }
+}
